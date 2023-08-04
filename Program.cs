17ac41b057e1562e57ac3606b21d9177460e22e9
Erik Rosemann <erik.rosemann@online.de>
2023-08-05 00:34:26 +0200

    add directory overview and entry description to output txt

--- Program.cs
+++ Program.cs
@@ -1,41 +1,60 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using Microsoft.Build.Construction;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string solutionFilePath = args[0]; // Pfad zur .sln-Datei
-        string outputFilePath = args[1]; // Pfad zur Ausgabedatei
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Fehler: Zu wenige Argumente.");
+            Console.WriteLine("Verwendung: dotnet run \".\\pfad\\zur\\dein.sln\" \".\\pfad\\zur\\ausgabe.txt\"");
+            return;
+        }
 
-        // Einlesen der .sln-Datei
+        string solutionFilePath = Path.GetFullPath(args[0]);
+        string outputFilePath = Path.GetFullPath(args[1]);
+
         SolutionFile solution = SolutionFile.Parse(solutionFilePath);
 
-        // Ignorierte Verzeichnisse
         string[] ignoredDirectories = new[] { "bin", "obj" };
+
+        int fileNumber = 0;
 
         using (StreamWriter sw = new StreamWriter(outputFilePath))
         {
+            // Erklärungstext
+            sw.WriteLine("Dieses Dokument enthält eine Übersicht über eine Visual Studio Lösung (.sln), inklusive aller Projekte, Ordner und Dateien.");
+            sw.WriteLine("Der erste Abschnitt ist das Inhaltsverzeichnis, gefolgt von den Inhalten jeder Datei.");
+            sw.WriteLine("Ignorierte Ordner: " + string.Join(", ", ignoredDirectories));
+            sw.WriteLine(new string('=', 80));
+
+            // Inhaltsverzeichnis der Ordnerstruktur
+            sw.WriteLine("Inhaltsverzeichnis:");
+            foreach (ProjectInSolution project in solution.ProjectsInOrder)
+            {
+                if (project.ProjectType != SolutionProjectType.KnownToBeMSBuildFormat) continue;
+                string projectDirectory = Path.GetDirectoryName(project.AbsolutePath);
+                WriteDirectoryContents(projectDirectory, sw, ignoredDirectories, Path.GetDirectoryName(solutionFilePath), indent: 2);
+            }
+            sw.WriteLine(new string('=', 80));
+
             // Gehe durch jedes Projekt in der Lösung
             foreach (ProjectInSolution project in solution.ProjectsInOrder)
             {
-                // Nur C#-Projekte beachten
                 if (project.ProjectType != SolutionProjectType.KnownToBeMSBuildFormat) continue;
 
                 string projectDirectory = Path.GetDirectoryName(project.AbsolutePath);
 
-                // Gehe durch jede .cs-Datei im Projektverzeichnis und Unterordnern
-                foreach (string file in Directory.EnumerateFiles(projectDirectory, "*.cs", SearchOption.AllDirectories))
+                foreach (string file in Directory.GetFiles(projectDirectory, "*.cs", SearchOption.AllDirectories))
                 {
-                    // Ignoriere Dateien in "bin" und "obj" Verzeichnissen
                     if (ignoredDirectories.Any(ignored => file.Contains(Path.DirectorySeparatorChar + ignored + Path.DirectorySeparatorChar))) continue;
 
-                    // Schreibe den Dateipfad
-                    sw.WriteLine(file);
+                    fileNumber++;
+                    string relativePath = Path.GetRelativePath(Path.GetDirectoryName(solutionFilePath), file);
+                    sw.WriteLine($"Datei #{fileNumber}: {relativePath}");
 
-                    // Schreibe den Dateiinhalt
                     try
                     {
                         string fileContent = File.ReadAllText(file);
@@ -46,12 +65,32 @@ class Program
                         Console.WriteLine($"Fehler beim Lesen der Datei: {file}. Fehler: {ex.Message}");
                     }
 
-                    // Trennzeile für die Lesbarkeit
-                    sw.WriteLine(new string('-', 80));
+                    // Saubere Trennung zwischen den Dateien
+                    sw.WriteLine(new string('=', 80));
                 }
             }
         }
 
         Console.WriteLine($"Die Pfade und Inhalte der Dateien wurden erfolgreich in {outputFilePath} geschrieben.");
+    }
+
+    private static void WriteDirectoryContents(string directoryPath, StreamWriter writer, string[] ignoredDirectories, string rootPath, int indent)
+    {
+        string indentation = new string(' ', indent);
+
+        foreach (string dir in Directory.GetDirectories(directoryPath))
+        {
+            if (ignoredDirectories.Any(ignored => dir.EndsWith(ignored))) continue;
+
+            string relativeDirPath = Path.GetRelativePath(rootPath, dir);
+            writer.WriteLine($"{indentation}{relativeDirPath}");
+            WriteDirectoryContents(dir, writer, ignoredDirectories, rootPath, indent + 2);
+        }
+
+        foreach (string file in Directory.GetFiles(directoryPath))
+        {
+            string relativeFilePath = Path.GetRelativePath(rootPath, file);
+            writer.WriteLine($"{indentation}{relativeFilePath}");
+        }
     }
 }
